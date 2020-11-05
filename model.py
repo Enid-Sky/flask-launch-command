d2dcc72a0e94ecde2dbcff4d1450efc9af94cd79
Enid-Sky <enid.sky@protonmail.com>
2020-11-05 11:50:26 -0800

    add user class

--- model.py
+++ model.py
@@ -4,3 +4,47 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 db = SQLAlchemy()
+
+
+class User(db.Model):
+    """User details"""
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer,
+                        primary_key=True,
+                        unique=True,
+                        autoincrement=True)
+
+    fname = db.Column(db.String(50), nullable=False)
+    lname = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(50), nullable=False)
+    mobile_number = db.Column(db.String(15), nullable=False, unique=True)
+
+    def __repr__(self):
+        """Show infor about user"""
+
+        return f"<User info = {self.user_id}, {self.fname}, {self.lname}>"
+
+
+def connect_to_db(flask_app, db_uri='postgresql:///launchcommand', echo=True):
+    """Connect the database to the Flask app"""
+
+    flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    flask_app.config['SQLALCHEMY_ECHO'] = echo
+    flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print('Connected to the db!')
+
+
+if __name__ == '__main__':
+    from server import app
+    connect_to_db(app)
+
+    # Call connect_to_db(app, echo=False) if your program output gets
+    # too annoying; this will tell SQLAlchemy not to print out every
+    # query it executes.
