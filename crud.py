fa199c6c3e09be397fe80c649edc8cdd13c7ee96
Enid-Sky <enid.sky@protonmail.com>
2020-11-05 21:10:50 -0800

    add function to create a user

--- crud.py
+++ crud.py
@@ -1,5 +1,17 @@
 """CRUD operations"""
 
 
-from model import db, connect_to_db
-# add any classes above
+from model import db, User, UpcomingLaunch, myLaunch, connect_to_db
+
+
+def create_user(fname, lname, email, password, mobile_number):
+
+    # create a user
+    user = User(fname=fname, lname=lname, email=email,
+                password=password, mobile_number=mobile_number)
+
+    # add user to database
+    db.session.add(user)
+    db.session.commit()
+
+    return user
