b801c67f078362cbdeae76395605b4a0b326152e
Enid-Sky <enid.sky@protonmail.com>
2020-11-05 21:11:27 -0800

    add function to create

--- crud.py
+++ crud.py
@@ -15,3 +15,16 @@ def create_user(fname, lname, email, password, mobile_number):
     db.session.commit()
 
     return user
+
+
+def crate_upcoming_launch(launch_api_id, name, status_name, window_start, mission_description, pad_location, image):
+
+    # create a launch
+    upcoming_launch = UpcomingLaunch(launch_api_id=launch_api_id, name=name, status_name=status_name,
+                                     window_start=window_start, mission_description=mission_description, pad_loaction=pad_location, image=image)
+
+    # save launch to database
+    db.session.add(upcoming_launch)
+    db.session.commit()
+
+    return upcoming_launch
