de632b2243e1ab9c54f1dd1c06e06069136bbae6
Enid-Sky <enid.sky@protonmail.com>
2020-11-05 20:25:13 -0800

    add route for upcoming launches

--- server.py
+++ server.py
@@ -2,9 +2,9 @@
 
 from jinja2 import StrictUndefined
 # import crud
-# from model import connect_to_db
-from flask import (Flask, render_template, request, flash, session,
-                   redirect, jsonify)
+from model import connect_to_db
+from flask import (Flask, render_template, request, flash, session, redirect)
+import requests
 
 
 app = Flask(__name__)
@@ -14,11 +14,47 @@ app.secret_key = "launch"
 
 app.jinja_env.undefined = StrictUndefined
 
+#######################################
+#                                     #
+#                                     #
+#             ROUTES                  #
+#                                     #
+#                                     #
+#######################################
+
 
 @app.route("/")
 def homepage():
     """ Homepage route"""
     return render_template('homepage.html')
+
+
+@app.route("/upcoming")
+def upcoming_results():
+    """ API results for upcoming launches route"""
+
+    res = requests.get(
+        "https://ll.thespacedevs.com/2.0.0/launch/upcoming/?limit=5/?format=json")
+
+    data = res.json()
+
+    launches = data['results']
+
+    # return render_template('upcoming_launches.html', data=data, results=launches)
+    return render_template('upcoming_launches.html', data=data, id=launches[0]["name"])
+
+
+# @app.route("/register_user", methods=['POST'])
+# def register_user():
+#     """ Regiter user and save to database"""
+
+# fname = request.form.get('firstName')
+# lname = request.form.get('lastName')
+# email = request.form.get('email')
+# password = request.form.get('password')
+# phone = request.form.get('phone')
+
+# return render_template('<name>.html')
 
 
 # @app.route("/login")
@@ -31,13 +67,13 @@ def homepage():
 # def register_user():
 #     """ Regiter user and save to database"""
 
-    # fname = request.form.get('firstName')
-    # lname = request.form.get('lastName')
-    # email = request.form.get('email')
-    # password = request.form.get('password')
-    # phone = request.form.get('phone')
+# fname = request.form.get('firstName')
+# lname = request.form.get('lastName')
+# email = request.form.get('email')
+# password = request.form.get('password')
+# phone = request.form.get('phone')
 
-    # return render_template('<name>.html')
+# return render_template('<name>.html')
 
 
 # @app.route("/logout")
@@ -47,10 +83,6 @@ def homepage():
 # @app.route("/upcoming_launches")
 
 # @app.route("/saved_launches")
-
-
 if __name__ == '__main__':
-    # connect_to_db(app)
-    app.run(host='0.0.0.0', debug=True)
-
-    # , port=5002
+    connect_to_db(app)
+    app.run(host='0.0.0.0', port=5000, debug=True)
