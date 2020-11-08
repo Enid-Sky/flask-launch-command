8dfcb8ed29225ff7d5f9772dbc2d7a7fa59ca437
Enid-Sky <enid.sky@protonmail.com>
2020-11-07 20:51:50 -0800

    comment out print statements - remove later

--- api.py
+++ api.py
@@ -6,5 +6,16 @@ def upcoming_launch_api():
     res = requests.get(
         "https://ll.thespacedevs.com/2.0.0/launch/upcoming/?limit=5/?format=json")
 
+    # data needs to be used in server route and to seed database?
     data = res.json()['results']
+
+    # for launch in data:
+
+    #     (launch['name'])
+    #     (launch['status']['name'])
+    #     (launch['window_start'])
+    #     (launch['mission']['description'])
+    #     (launch['pad']['location']['name'])
+    #     (launch['image'])
+
     return data
