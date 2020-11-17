e4303a9cb07c4d851f0356c7ce9a1ccdf711a369
Enid-Sky <enid.sky@protonmail.com>
2020-11-16 21:01:57 -0800

    add news api

--- api.py
+++ api.py
@@ -6,7 +6,18 @@ def upcoming_launch_api():
     res = requests.get(
         "https://ll.thespacedevs.com/2.0.0/launch/upcoming/?limit=5/?format=json")
 
-    # data needs to be used in server route and to seed database?
     data = res.json()['results']
 
     return data
+
+
+def news_api():
+    news_res = requests.get(
+        "https://test.spaceflightnewsapi.net/api/v2/articles?_limit=10")
+
+    news_data = news_res.json()
+
+    return(news_data)
+
+    ########################
+    # print(news_data[0]['title'])
