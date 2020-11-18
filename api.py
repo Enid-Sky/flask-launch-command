848350cbfe9eef0c60e4c9cb620be62549b8154a
Enid-Sky <enid.sky@protonmail.com>
2020-11-17 16:24:21 -0800

    remove duplicate function

--- api.py
+++ api.py
@@ -9,12 +9,3 @@ def upcoming_launch_api():
     data = res.json()['results']
 
     return data
-
-
-def upcoming_launch_api():
-    res = requests.get(
-        "https://ll.thespacedevs.com/2.0.0/launch/upcoming/?limit=5/?format=json")
-
-    data = res.json()['results']
-
-    return data
