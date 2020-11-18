1d90c7619a682ce918b2ad6635eb30088286c28c
Enid-Sky <enid.sky@protonmail.com>
2020-11-17 16:21:21 -0800

    remove print statements

--- api.py
+++ api.py
@@ -11,24 +11,10 @@ def upcoming_launch_api():
     return data
 
 
-def news_api():
-    news_res = requests.get(
-        "https://test.spaceflightnewsapi.net/api/v2/articles?_limit=10")
+def upcoming_launch_api():
+    res = requests.get(
+        "https://ll.thespacedevs.com/2.0.0/launch/upcoming/?limit=5/?format=json")
 
-    news_data = news_res.json()
+    data = res.json()['results']
 
-    ########################
-    # print(news_data[0]['title'])
-    # print(news_data)
-
-    # return(news_data)
-
-    # for dic in news_data:
-    #     for key in dic:
-    #         print(dic[key])
-
-    for dic in news_data:
-        title, url, image, news_site, summary, date = (
-            dic['title'], dic['url'], dic['imageUrl'], dic['newsSite'], dic['summary'], dic['publishedAt'])
-
-        print(title, url, image, news_site, summary, date)
+    return data
