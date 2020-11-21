536bea1863f06121ac17b97f0c44233607e4f7ae
Enid-Sky <enid.sky@protonmail.com>
2020-11-20 23:08:30 -0800

    add fetch function

--- static/app.js
+++ static/app.js
@@ -1,47 +1,53 @@
 'use strict';
 
+function getData() {
+    fetch('/data')
+        .then(function (response) {
+            return response.json();
+        })
+        .then (function (text) {
+            console.log('GET response:');
+            console.log(text.date);
+        });
+        test = text.date
+        document.getElementById("date").innerHTML = `${test}`
+        return test;     
+};
 
-// Set the date we're counting down to
-fetch('https://ll.thespacedevs.com/2.0.0/launch/upcoming/?limit=5/?format=json')
-.then((res) => {
-    let data = res.json();
-    let results = data['results']
-    console.log(results);
-    return results;
-})
 
-.then((results) => {
-    let nextLaunch = results['start_window'];
-    
+function countdown(data) {
+    const countDownDate = new Date(data).getTime();
+    console.log('window')
+    console.log(window.launch.time);
 
-    const countDownDate = new Date(nextLaunch).getTime();
     // Update the count down every 1 second
-    const x = setInterval(function() {
+    const currentCountdown = setInterval(function() {
 
     // Get today's date and time
     const now = new Date().getTime();
         
-    // Find the distance between now and the count down date
-    const distance = countDownDate - now;
+    // Time difference between now and the countdown time
+    const timeRemaining = countDownDate - now;
         
     // Time calculations for days, hours, minutes and seconds
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
         
-    // Output the result in an element with id="demo"
-    document.getElementById("demo").innerHTML = days + "d : " + hours + "h "
-    + minutes + "m " + seconds + "s ";
+    // Output the result in an element with id="countdown
+    document.getElementById("countdown").innerHTML = `${days} : ${hours} : ${minutes} : ${seconds}`;
         
-    // If the count down is over, write some text 
-        if (distance < 0) {
-            clearInterval(x);
-            document.getElementById("demo").innerHTML = "ENDED";
+    // If the count down is over, 
+        if (timeRemaining < 0) {
+            clearInterval(currentCountdown);
+            document.getElementById("countdown").innerHTML = "ENDED";
         }
     }, 1000);
 
-})
+}
+
+
 
 
 
@@ -55,3 +61,8 @@ $("#follow_input").on('click', () => {
         $("#follow_input").text("Follow");
     }
 });
+
+
+// FUNCTION CALLS
+countdown()
+getData()
