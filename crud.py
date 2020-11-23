b00d46c8d8904100ee5e88d98a1b86994c4629b7
Enid-Sky <enid.sky@protonmail.com>
2020-11-22 16:57:35 -0800

    update get upcoming launch function to next launch

--- crud.py
+++ crud.py
@@ -114,7 +114,7 @@ def get_all_saved_by_id(user_id):
 def get_next_upcoming_launch():
     """Return next upcoming launch to display in countdown"""
 
-    next_launch_time = Upcominglaunch.query.get(2)
+    next_launch_time = Upcominglaunch.query.get(1)
 
     return next_launch_time
 
