95af16485fbb2866920483d7b0e2968c460dc0b8
Enid-Sky <enid.sky@protonmail.com>
2020-11-29 17:56:29 -0800

    keep for testing

--- reminders.py
+++ reminders.py
@@ -1,33 +1,21 @@
 from twilio.rest import Client
 import os
 
-send_to = os.environ['USER_NUMBER']
-twilio = os.environ['TWILIO_NUMBER']
-account_sid = os.environ['ACCOUNT_SID']
-auth_token = os.environ['AUTH_TOKEN']
-client = Client(account_sid, auth_token)
 
-message = client.messages.create(
-    to=send_to,
-    from_=twilio,
-    body='STARLINK NOV 28 2020'
-)
-# reminder = request.form.get('reminder')
-flash(f'A reminder has been sent.')
-print(message.sid)
+def reminder():
+    send_to = os.environ['USER_NUMBER']
+    twilio = os.environ['TWILIO_NUMBER']
+    account_sid = os.environ['ACCOUNT_SID']
+    auth_token = os.environ['AUTH_TOKEN']
 
-# account_sid = os.environ['ACCOUNT_SID']
-# auth_token = os.environ['AUTH_TOKEN']
+    client = Client(account_sid, auth_token)
 
-# print(account_sid)
-# print(auth_token)
+    message = client.messages.create(
+        to=send_to,
+        from_=twilio,
+        body=f'Upcoming launch reminder.'
+    )
 
-# client = Client(account_sid, auth_token)
+    print(message.sid)
 
-
-# message = client.messages.create(
-#     body="STARLINK NOV 28 2020",
-#     to='+13108823819',
-#     from_='+13158193842'
-# )
-# print(message.sid)
+    return message
