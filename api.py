80f0cd57f60ee486255005aaf38a10e5bb794534
Enid-Sky <enid.sky@protonmail.com>
2020-12-06 18:07:15 -0800

    update to changed API URL

--- api.py
+++ api.py
@@ -4,7 +4,7 @@ import json
 
 def upcoming_launch_api():
     res = requests.get(
-        "https://ll.thespacedevs.com/2.0.0/launch/upcoming/?limit=5/?format=json")
+        "https://ll.thespacedevs.com/2.0.0/launch/upcoming/?format=json&limit=10&offset=3")
 
     data = res.json()['results']
 
